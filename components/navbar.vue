7c0cc2472259772c543b07678f6e1437e67f0530
LynaBadaoui <il_badaoui@esi.dz>
2023-05-02 11:28:19 +0100

    added logo to navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-app-bar app color="white" flat hide-on-scroll prominent class="navbar">
+  <v-app-bar app color="white" flat hide-on-scroll prominent>
       <v-toolbar-title>
-        <!-- <v-img src="/images/logo-color 1.png" > </v-img> -->
+        <v-img src="/images/logo-color 1.png" class="img"> </v-img>
  
       </v-toolbar-title>
-      <div class="test ml-auto">
+      <div class="ml-auto button-group">
         <v-btn text to="/" class="navbar-button">Accueil</v-btn>
         <v-btn text to="/about" class="navbar-button">A propos</v-btn>
         <v-btn text to="/programme" class="navbar-button">Le Programme</v-btn>
@@ -12,7 +12,7 @@
         <v-btn text to="/faq" class="navbar-button">FAQ</v-btn>
         <v-btn text to="/contact" class="navbar-button">Contact</v-btn>
         <v-btn elevation="0" class=" navbar-button program">Rejoindre le programme</v-btn>
-        <v-icon></v-icon>
+        <v-icon class="icon"> mdi-web</v-icon>
       </div>
     </v-app-bar>
 </template>
@@ -24,30 +24,48 @@ export default {
 </script>
 
 <style >
+
+.button-group {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.button-group > * {
+  color:#2D2D2D !important;
+  margin-left: 16px;
+  margin-right: 16px;
+  font-size: 9pt !important;
+
+}
+
+.navbar-button:hover{
+  background-color: transparent !important;
+}
+.navbar-button::before{
+  background-color: transparent !important;
+}
 .program {
   border: solid;
   border-color: #046380;
   background-color: white !important;
   color: #046380 !important;
   border-width: 1px;
-  margin:-20px;
-}
-.navbar-button:hover{
-  background-color: transparent !important;
-}
-
-.navbar-button::before{
-  background-color: transparent !important;
-}
-.navbar-button:focus{
-    border:solid !important;
-    border-bottom: #046380 !important;
+  border-radius: 10px !important;
 }
 .program:hover{
   background-color: #046380 !important;
   color: white !important;
+  
 }
-.navbar{
-  align-items: right !important;
+
+.icon.mdi:before {
+  color: #2D2D2D !important;
+  font-size: 25px !important;
+}
+
+.img{
+  margin-bottom: -20px;
 }
 </style>
