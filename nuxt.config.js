cf027af596a25a055a0b5a5fe440cc42fe915baf
Kaoutar Djaoud <kaoutar.djaoud@gmail.com>
2023-05-07 11:41:39 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,5 +78,20 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
 }
