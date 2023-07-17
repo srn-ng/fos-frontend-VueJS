8571dd1e5abd9032944dc4a841bce85db83b9981
kaoutarDJAOUD <ek_djaoud@esi.dz>
2023-07-17 12:12:13 +0100

    [DEV] Major refacto

---
+++ components/Contact.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container mt-15">
+    <v-row justify="center">
+      <v-col
+        class="contact-info d-flex align-center justify-center p-0 mr-md-n12 mr-lg-n10"
+        cols="10"
+        md="4"
+      >
+        <v-card class="px-md-5 py-md-15" color="rgba(4, 99, 128)">
+          <v-card-title class="white-title">Contact info</v-card-title>
+          <v-card-text>
+            <ul>
+              <li>
+                <v-icon color="white" class="mr-1"> mdi-map-marker </v-icon>
+                <span>137 ter Bd Colonel Krim Belkacem, Algiers</span>
+              </li>
+              <li>
+                <v-icon color="white" class="mr-1"> mdi-mail </v-icon>
+                <span>contact@finishoffschool.com</span>
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="10" md="5" class="ml-md-n5">
+        <v-card class="px-7 px-md-15 py-md-10" color="rgb(219, 218, 218)">
+          <v-card-title class="blue-title">Contactez-nous</v-card-title>
+          <v-card-text>
+            <v-form v-model="valid">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                :counter="10"
+                label="Nom"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="lastname"
+                :rules="nameRules"
+                :counter="10"
+                label="Prénom"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+              ></v-text-field>
+
+              <v-textarea
+                v-model="message"
+                variant="filled"
+                auto-grow
+                label="Message"
+                rows="4"
+                row-height="30"
+                shaped
+                required
+              ></v-textarea>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    firstname: '',
+
+    lastname: '',
+    nameRules: [
+      (v) => !!v || 'Name is required',
+      (v) => v.length <= 10 || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      (v) => !!v || 'E-mail is required',
+      (v) => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+    message: '',
+  }),
+}
+</script>
+
+<style scoped>
+.blue-title {
+  color: #046380;
+  font-weight: bold;
+}
+
+.white-title {
+  color: #fff;
+  font-weight: bold;
+}
+.contact-form {
+  padding: 5rem;
+}
+.contact-info {
+  z-index: 1000;
+}
+ul {
+  color: white;
+}
+li {
+  position: relative;
+  list-style: none;
+  display: flex;
+  margin: 25px 0;
+  cursor: pointer;
+  align-items: flex-start;
+  color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,110 @@
 <template>
-  <div class="pb-5 h-100">
-    <v-row justify="center" class="pb-5">
-      <v-col cols="10" md="4" class="d-flex align-center">
-        <div class="text-container">
-          <h4 style="display: inline-block">
-            Apprendre.
+  <v-app>
+    <navbar @change-drawer="handleEmit"></navbar>
+    <v-main>
+      <home id="home" />
+      <overview id="about" />
+      <contenu id="contenu" />
+      <calendar id="calendar" />
+      <faq id="faq" />
+      <contact id="contact" />
+      <partners id="partners" />
+    </v-main>
+    <v-navigation-drawer v-model="drawer">
+      <v-list nav dense>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
 
-            <br />
-            Evoluer.
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
 
-            <br />
-            Réussir.
-          </h4>
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
 
-          <h3 class="my-5">The Finish Off SCHOOL</h3>
-          <v-divider class="my-4" color="white" width="15%"></v-divider>
-          <p style="margin-top: 10%">
-            Le premier programme de formation pluridisciplinaire de haut niveau
-            conçu pour accompagner les jeunes algériens dans leurs premiers pas
-            vers le monde professionnel.
-          </p>
-        </div>
-      </v-col>
-      <!-- <v-col cols="1"></v-col> -->
-      <v-col cols="10" md="4" style="margin-top: 5%">
-        <v-row>
-          <v-col cols="6" style="margin-top: 20%">
-            <v-card rounded>
-              <v-img
-                src="/images/Rectangle image 1.jpg"
-                
-              ></v-img>
-            </v-card>
-          </v-col>
-          <v-col cols="6">
-            <v-card rounded>
-              <v-img
-                src="/images/Rectangle image 2.jpg"
-                
-              ></v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-  </div>
-</template>
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer app absolute color="#D9D9D9" height="128px">
+      <v-img src="/images/logo.png" class="logo"></v-img>
+    </v-footer>
+  </v-app>
+</template> 
+  <script>
+import navbar from '../layouts/navbar.vue'
+import home from '../components/Home.vue'
+import overview from '../components/Programm.vue'
+import calendar from '../components/Calendar.vue'
 
+import contenu from '../components/ProgrammContent.vue'
+import partners from '../components/Partners.vue'
 
-<script>
-import { VImg } from 'vuetify/lib'
+import faq from '../components/faq.vue'
+import contact from '../components/Contact.vue'
 
 export default {
-  name: 'IndexPage',
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Accueil',
+          to: '/index',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
   components: {
-    VImg,
+    navbar,
+    home,
+    overview,
+    calendar,
+    contenu,
+    partners,
+    faq,
+    contact,
+  },
+  methods: {
+    handleEmit() {
+      console.log('emit received')
+      this.drawer = !this.drawer
+    },
   },
 }
 </script>
-
-<style>
-@media only screen and (max-width: 600px) {
-  /* Apply style to h3 with class 'small-screen' */
-  .small-screen {
-    font-size: 1.5rem;
-    /* Add any other styles you want to apply to h3 on small screens */
-  }
-}
-
-@media only screen and (min-width: 601px) {
-  /* Apply style to h3 with class 'large-screen' */
-  .large-screen {
-    font-size: 2.5rem;
-    display: inline-block;
-    text-align: center;
-    /* Add any other styles you want to apply to h3 on large screens */
-  }
-}
-
-#algiers-line {
-  position: absolute;
-  left: 0;
-  right: 0;
-  margin: auto;
-  top: 50%;
-  transform: translateY(50%);
-  width: 10%;
-}
-
-.text-container {
-  color: #fff;
-}
-
-.text-container h3 {
-  font-family: 'Montserrat semibold', sans-serif;
-  font-size: 2.5rem;
-}
-
-.text-container h4 {
-  font-family: 'Montserrat bold', sans-serif;
-  font-size: 1.5rem;
-}
-
+  <style >
 * {
   font-family: 'Montserrat', sans-serif;
 }
-
-.background-image-container {
-  background-image: url('/images/Background-Polygon.png');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center center;
+.logo {
+  max-height: 75px;
+  max-width: 75px;
 }
 </style>
+  
+  
+  
